8d0653b243cd9884ab8e4cdffb00ad7a870c2785
kimsuankim <144988337+kimsuankim@users.noreply.github.com>
2025-05-29 00:13:35 +0900

    2

--- main.c
+++ main.c
@@ -29,20 +29,20 @@ int dequeue(int a[], int *front, int rear){
 
 int main(int argc, char *argv[]) {
 	//카드 뒤집기 게임
-	Queue player; //player줄 생성
-	player.front = player.rear=-1; //공백큐, 초기화
-	//1.5장의 카드 번호 입력, 플레이어 FIFO순으로 카드 받음=플레이어는 줄에 들어옴과 동시에 카드를 받음 
+	Queue card; //card줄 생성 
+	card.front = card.rear=-1; //공백큐, 초기화
+	//1.5장의 카드 번호 입력, 플레이어는 카드 받고 차례대로 저장
 	int i, cnum;
 	for(i=0;i<SIZE;i++){
 		printf("%d번째 카드번호 입력: ",i+1); //사용자가 숫자를 입력 
 		scanf("%d", &cnum);
-		enqueue(player.data,&player.rear, cnum); //큐에 넣기 
+		enqueue(card.data,&card.rear, cnum); //큐에 넣기 
 	}
 	
-	//2.앞에서부터 공개
+	//2.앞에서부터 공개(FIFO)
 	printf("카드 공개 순서:");
 	for(i=0;i<SIZE;i++){
-		cnum=dequeue(player.data,&player.front, player.rear); //큐에서 하나씩 앞부터 빼기 
+		cnum=dequeue(card.data,&card.front, card.rear); //큐에서 하나씩 앞부터 빼기 
 		printf("%d ",cnum);
 	}
 	//3. 배열 큐 사용, 
